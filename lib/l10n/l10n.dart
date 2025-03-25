9867e1b1a06a9482249a76179027a95791075b71
Cristian Arevalo <csarevalo1@gmail.com>
2025-03-24 21:42:09 -0700

    added translations and more comments

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -5,5 +5,6 @@ export 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 /// An extension to provide localized wording
 extension AppLocalizationsX on BuildContext {
+  /// Use this to access the localized words and phrases
   AppLocalizations get l10n => AppLocalizations.of(this);
 }
